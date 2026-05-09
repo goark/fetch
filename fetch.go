2a18ca7b8354eb300e5bbf7d4873b56125dea832
Spiegel <spiegel.im.spiegel@gmail.com>
2026-05-09 21:50:27 +0900

    docs: improve API comments and update copyright years

--- fetch.go
+++ fetch.go
@@ -7,28 +7,41 @@ import (
 	"net/url"
 )
 
+// RequestOpts applies options to an HTTP request.
+//
+// The returned request value allows option functions to replace the request,
+// for example when changing context with req.WithContext.
 type RequestOpts func(*http.Request) *http.Request
 
-// Client is inteface class for HTTP client.
+// Client represents a fetch client.
 type Client interface {
-	// Deprecated: Should use GetWithContext() method instead of Get() method.
+	// Get sends a GET request.
+	// Deprecated: Use GetWithContext instead.
 	Get(u *url.URL, opts ...RequestOpts) (Response, error)
+	// GetWithContext sends a GET request with context.
 	GetWithContext(ctx context.Context, u *url.URL, opts ...RequestOpts) (Response, error)
-	// Deprecated: Should use PostWithContext() method instead of Post() method.
+	// Post sends a POST request.
+	// Deprecated: Use PostWithContext instead.
 	Post(u *url.URL, payload io.Reader, opts ...RequestOpts) (Response, error)
+	// PostWithContext sends a POST request with context.
 	PostWithContext(ctx context.Context, u *url.URL, payload io.Reader, opts ...RequestOpts) (Response, error)
 }
 
-// Response is inteface class for HTTP response.
+// Response wraps an HTTP response.
 type Response interface {
+	// Request returns the original request.
 	Request() *http.Request
+	// Header returns response headers.
 	Header() http.Header
+	// Body returns the response body reader.
 	Body() io.ReadCloser
+	// Close drains and closes the response body.
 	Close() error
+	// DumpBodyAndClose reads all body bytes and closes the response body.
 	DumpBodyAndClose() ([]byte, error)
 }
 
-/* Copyright 2023-2025 Spiegel
+/* Copyright 2023-2026 Spiegel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
